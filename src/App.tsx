224c78dcf7721f59db02c23bdcf6f4b7798deb83
Lino09 <onosmx@gmail.com>
2022-10-18 22:18:00 -0500

    fix: background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react"
 import FormOverlay from "./Components/FormOverlay"
+import bgImage from '/src/assets/images/bg.jpg'
 
 function App() {
   const [isFormOpen, isFormOpenSet] = useState(true)
   return (
-    <div className="App h-[100vh] w-full" style={{backgroundImage: "url('src/assets/images/bg.jpg')", backgroundSize: 'cover'}}>
+    <div className="App h-[100vh] w-full" style={{backgroundImage: `url(${bgImage})`, backgroundSize: 'cover'}}>
       {/* <button onClick={() => isFormOpenSet(!isFormOpen)} className="border p-2">Toggle</button> */}
       {isFormOpen && <FormOverlay onClose={() => isFormOpenSet(false)} ></FormOverlay>}
     </div>
